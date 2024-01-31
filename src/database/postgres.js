11d9384ccb9bf5502000b5649a00229641f591c6
Luis Gustavo Quispe Copa <57078251+luiscopa3000@users.noreply.github.com>
2024-01-31 15:33:48 -0400

    Update postgres.js

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('bsthwsfbpulvq1wpc0gv', 'uduwnvnfzz5zxjwlgjgi', 'ZSxUxCBwDGFzx1ufAFxKAdEtDwa8Km', {
     host: 'bsthwsfbpulvq1wpc0gv-postgresql.services.clever-cloud.com',
-    port: '50013'
+    port: '50013',
     dialect: 'postgres',
     define: {
         timestamps: false, // Esto evita que Sequelize agregue 'created_at' y 'updated_at' automáticamente
