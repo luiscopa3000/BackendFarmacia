b46d091177a613a984062bfad7eb190599627189
Luis Gustavo Quispe Copa <57078251+luiscopa3000@users.noreply.github.com>
2024-01-31 15:23:15 -0400

    Update postgres.js
    Se cambia el host de la base de datos

--- src/database/postgres.js
+++ src/database/postgres.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('farmacia', 'postgres', '123456', {
-    host: 'localhost',
+const sequelize = new Sequelize('bsthwsfbpulvq1wpc0gv', 'uduwnvnfzz5zxjwlgjgi', '50013', {
+    host: 'bsthwsfbpulvq1wpc0gv-postgresql.services.clever-cloud.com',
     dialect: 'postgres',
     define: {
         timestamps: false, // Esto evita que Sequelize agregue 'created_at' y 'updated_at' automáticamente
