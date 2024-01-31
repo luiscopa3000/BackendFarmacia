b3a61cc2a4313d95946452f268c4449bc8a7cf36
luiscopa3000 <plan300014@gmail.com>
2024-01-31 16:14:21 -0400

    Agrgacion del Cors

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 const express = require('express');
 const routes = require('./src/routers/useroutes'); // Importa las rutas desde el archivo routes.js
 const bodyParser = require('body-parser');
-
+const cors = require('cors')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Usa las rutas definidas en routes.js
 app.use(bodyParser.json());
+app.use(cors())
 app.use('/', routes);
 
 app.listen(PORT, () => {
