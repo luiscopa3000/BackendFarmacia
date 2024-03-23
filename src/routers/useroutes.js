825c5b268965cabf517906f1a22538e5ce209069
Luis Gustavo Quispe Copa <57078251+luiscopa3000@users.noreply.github.com>
2024-03-23 00:30:11 -0400

    Update useroutes.js

--- src/routers/useroutes.js
+++ src/routers/useroutes.js
@@ -143,14 +143,11 @@ router.post('/mostrar', async (req, res) => {
     }
 })
 // EndPoint para devolver las lista de productos del sucursal x
-router.post('/lsmedicament-sucx', verificarToken, async (req, res) => {
+router.post('/lsmedicament', verificarToken, async (req, res) => {
     const { id_sucursal } = req.body;
     try {
         // Busca todos los productos asociados a la sucursal mediante la relación definida en tu modelo
-        const productos = await Producto.findAll({
-            where: { id_sucursal },
-            // Puedes incluir más opciones aquí según tus necesidades
-        });
+        const productos = await Producto.findAll();
         res.json(productos);
     } catch (error) {
         console.error(error);
